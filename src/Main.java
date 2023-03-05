d339340542bc1a2107d1cff2b8ddf8f8a31926f1
Tikhonova Evgeniia <tikhonovae555@gmail.com>
2023-03-05 19:50:12 +0700

    Changes added

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int balance = 200;
+        int sumAdded = 1500;
+        boolean compare = sumAdded > 1000;
+        int bonus = compare ? 1 : 0;
+        int bonusRub = sumAdded * bonus / 100;
+        int finalBalance = balance + sumAdded + bonusRub;
+        System.out.println("Итоговый счёт " + finalBalance);
+        System.out.println("Количество бонусных рублей " + bonusRub);
+
+
     }
 }
